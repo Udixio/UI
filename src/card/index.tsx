613abc92a42f0d973105686855a557f26e311658
Joël <vigreux.joel@hotmail.com>
2023-12-14 19:56:42 +0100

    feat: add nx workspace

--- src/card/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from './card';
-export * from './CardMedia';
-export * from './CardHeader';
-export * from './CardAction';
-export * from './CardContent';
-
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from './button';
-export * from './code-preview';
-export * from './diviser';
-export * from './icon';
-export * from './tabs';
-export * from './utils';
-export * from './card';
