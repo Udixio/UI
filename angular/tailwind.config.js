6432db93dedca39d14644365d9a277e0487b8568
Joël <vigreux.joel@hotmail.com>
2023-12-20 15:55:32 +0100

    feat: config tailwind for udixio material

--- angular/tailwind.config.js
+++ angular/tailwind.config.js
@@ -1,5 +1,15 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
+const { createMaterialTheme } = require('@udixio/tailwind-material');
+
+const materialTailwind = createMaterialTheme({
+  colors: {
+    palette: {
+      primary: '#6750A4',
+    },
+  },
+  darkMode: 'class',
+});
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,7 +18,24 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    colors: {
+      ...materialTailwind.colors,
+    },
+    fontFamily: {
+      ...materialTailwind.fontFamily,
+    },
+    extend: {
+      boxShadow: {
+        1: '0px 1px 3px 1px rgba(0, 0, 0, 0.15), 0px 1px 2px 0px rgba(0, 0, 0, 0.30)',
+        2: '0px 2px 6px 2px rgba(0, 0, 0, 0.15), 0px 1px 2px 0px rgba(0, 0, 0, 0.30)',
+        3: '0px 1px 3px 0px rgba(0, 0, 0, 0.30), 0px 4px 8px 3px rgba(0, 0, 0, 0.15)',
+        4: '0px 2px 3px 0px rgba(0, 0, 0, 0.30), 0px 6px 10px 4px rgba(0, 0, 0, 0.15)',
+        5: '0px 4px 4px 0px rgba(0, 0, 0, 0.30), 0px 8px 12px 6px rgba(0, 0, 0, 0.15)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    ...materialTailwind.plugins,
+    require('@tailwindcss/container-queries'),
+  ],
 };
--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -35,11 +35,6 @@ module.exports = {
         4: '0px 2px 3px 0px rgba(0, 0, 0, 0.30), 0px 6px 10px 4px rgba(0, 0, 0, 0.15)',
         5: '0px 4px 4px 0px rgba(0, 0, 0, 0.30), 0px 8px 12px 6px rgba(0, 0, 0, 0.15)',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [
