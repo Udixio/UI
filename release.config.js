66ebe3d039ffd18e5cf8d259b9ae1d0773fc79bd
Joël VIGREUX <joel@udixio.fr>
2024-12-30 10:48:26 +0100

    feat: add commit validation script and update release config
    Add a script to validate commits and enforce rules for blocking major, minor, and patch changes. Update release configuration to include additional branches and customize the Semantic Release Git plugin for changelog management. These changes improve release workflows and align with branching strategies.

--- release.config.js
+++ release.config.js
@@ -1,12 +1,23 @@
 module.exports = {
   npmPublish: true,
   repositoryUrl: 'https://github.com/Udixio/UI.git',
-  branches: ['master', 'next', { name: 'beta', prerelease: true }],
+  branches: [
+    {
+      name: 'main',
+    },
+    {
+      name: 'develop',
+      prerelease: true,
+    },
+    {
+      name: 'hotfix/*',
+      prerelease: false,
+    },
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
     [
       '@semantic-release/github',
       {
@@ -18,7 +29,14 @@ module.exports = {
         ],
       },
     ],
-
-    '@semantic-release/git',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
