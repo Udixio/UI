5865037a2d793a7ca23bba0601e74075d45ddf27
Joël VIGREUX <joel@udixio.fr>
2025-01-13 10:06:17 +0100

    feat(carousel): add storybook stories and refactor component
    Added initial Storybook stories for Carousel with multiple variants. Refactored the component structure to improve maintainability by introducing a dedicated interface file and enhancing type safety. Simplified the component props handling and updated styling to align with new variants.

--- src/containment/carousel/Carousel.tsx
+++ src/containment/carousel/Carousel.tsx
@@ -1,31 +1,30 @@
-import React, { forwardRef, useRef } from 'react';
-import { StyleProps, StylesHelper } from '../../utils';
+import React, { useRef } from 'react';
+import { StylesHelper } from '../../utils';
 import { CarouselStyle } from './CarouselStyle';
-import classNames from 'classnames';
-import { AnimatePresence, motion } from 'framer-motion';
+import {
+  CarouselDefaultProps, CarouselElement,
+  CarouselExternalProps,
+  CarouselInternalState,
+  CarouselProps,
+} from './carousel.interface';
 
-export interface CarouselState {}
-
-export type CarouselElement =
-  | 'carousel'
-
-
-export interface CarouselProps
-  extends StyleProps<CarouselState, CarouselElement>,
-    CarouselState,
-    Omit<
-      React.HTMLAttributes<HTMLDivElement>,
-      'className'
-    > {
-}
-
-export const Carousel = forwardRef<HTMLDivElement, CarouselProps>((args, ref) => {
-  const { className, ...restProps }: CarouselProps = args;
+export const Carousel = ({
+  className,
+  variant = 'hero',
+  ref,
+  ...restProps
+}: CarouselProps) => {
   const getClassNames = (() => {
-    return StylesHelper.classNamesElements<CarouselState, CarouselElement>({
+    return StylesHelper.classNamesElements<
+      CarouselExternalProps & CarouselDefaultProps & CarouselInternalState,
+      CarouselElement
+    >({
       default: 'carousel',
       classNameList: [className, CarouselStyle],
-      states: {},
+      states: {
+        variant,
+        ref
+      },
     });
   })();
 
@@ -33,14 +32,9 @@ export const Carousel = forwardRef<HTMLDivElement, CarouselProps>((args, ref) =>
   const resolvedRef: React.RefObject<any> | React.ForwardedRef<any> =
     ref || defaultRef;
 
-
   return (
-    <div
-      className={getClassNames.carousel}
-      ref={resolvedRef}
-      {...restProps}
-    >
-
+    <div className={getClassNames.carousel} ref={resolvedRef} {...restProps}>
+      caroussel
     </div>
   );
-});
+};
