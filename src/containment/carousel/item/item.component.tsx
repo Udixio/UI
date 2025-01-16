51b4c07368b19fbc256d51b9c22948972bee52b2
Joël VIGREUX <joel@udixio.fr>
2025-01-16 13:30:02 +0100

    feat(carousel): add dynamic visibility percentage tracking
    Introduced visibility percentage calculations for carousel items using framer-motion’s useScroll and useMotionValueEvent hooks. Replaced the previous isExpanded logic with visibilityPercentage to enable more precise control over item styles and interactions. Updated item components and styles to reflect these changes, including dynamically displaying visibility percentages.

--- src/containment/carousel/item/item.component.tsx
+++ src/containment/carousel/item/item.component.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { carouselHelper, ItemProps } from './item.interface';
 
 export const Item = ({
-  isExpanded,
   className,
   children,
+  visibilityPercentage,
   ...restProps
 }: ItemProps) => {
+  const isExpanded = visibilityPercentage >= 50;
   const styles = carouselHelper.getStyles({
     isExpanded,
+    visibilityPercentage,
   });
 
   return (
     <div className={styles.item} {...restProps}>
       {children}
+      <p
+        className={
+          'text-display-large absolute text-on-surface bg-surface -translate-x-1/2 left-1/2 top-1/2'
+        }
+      >
+        {Math.round(visibilityPercentage)}
+      </p>
     </div>
   );
 };
