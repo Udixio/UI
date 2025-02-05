c4f34df827216066ce428bc4904cf0734641a6ba
Joël VIGREUX <joel@udixio.fr>
2025-02-05 10:06:38 +0100

    feat: add horizontal scrolling support in SmoothScroll
    Introduced an optional orientation prop to enable horizontal scrolling alongside vertical scrolling. Updated logic and styles to handle horizontal content and container sizes dynamically. Simplified imports and removed unused code for better readability and maintenance.

--- src/effects/smooth-scroll.effect.tsx
+++ src/effects/smooth-scroll.effect.tsx
@@ -1,38 +1,27 @@
-import {
-  ReactNode,
-  useCallback,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
-import {
-  motion,
-  useMotionValueEvent,
-  useScroll,
-  useSpring,
-  useTransform,
-} from 'framer-motion';
+import { ReactNode, useLayoutEffect, useRef, useState } from 'react';
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
+import { classNames } from '../utils';
 
-export const SmoothScroll = ({ children }: { children: ReactNode }) => {
+export const SmoothScroll = ({
+  children,
+  orientation = 'vertical',
+  maxSize,
+}: {
+  children: ReactNode;
+  orientation?: 'vertical' | 'horizontal';
+  maxSize?: number;
+}) => {
   // scroll container
 
   const ref = useRef<HTMLDivElement>(null);
 
   const contentRef = useRef(null);
 
-  // page scrollable height based on content length
   const [contentScrollSize, setContentScrollSize] = useState({
     width: 0,
     height: 0,
   });
   const [containerSize, setContainerSize] = useState({ width: 0, height: 0 });
-
-  // update scrollable height when browser is resizing
-  const resizeScrollHeight = useCallback((entries) => {
-    for (let entry of entries) {
-      setContentScrollSize(entry.contentRect.height);
-    }
-  }, []);
 
   // observe when browser is resizing
   useLayoutEffect(() => {
@@ -48,8 +37,8 @@ export const SmoothScroll = ({ children }: { children: ReactNode }) => {
         } else if (entry.target === contentRef.current) {
           // Mise à jour pour le deuxième élément
           setContentScrollSize({
-            width,
-            height,
+            width: maxSize ?? width,
+            height: maxSize ?? height,
           });
         }
       }
@@ -62,67 +51,61 @@ export const SmoothScroll = ({ children }: { children: ReactNode }) => {
     contentRef && resizeObserver.observe(contentRef.current);
 
     return () => resizeObserver.disconnect();
-  }, [contentRef, resizeScrollHeight]);
+  }, [contentRef]);
 
-  const { scrollYProgress, scrollY } = useScroll({ container: ref }); // measures how many pixels user has scrolled vertically
-  // as scrollY changes between 0px and the scrollable height, create a negative scroll value...
-  // ... based on current scroll position to translateY the document in a natural way
-
-  useMotionValueEvent(scrollYProgress, 'change', (latestValue) => {
-    console.log(latestValue, scrollY.get(), contentScrollSize);
+  const { scrollYProgress, scrollY, scrollXProgress, scrollX } = useScroll({
+    container: ref,
   });
 
   const transform = useTransform(
-    scrollYProgress,
+    orientation === 'vertical' ? scrollYProgress : scrollXProgress,
     [0, 1],
-    [0, -contentScrollSize.height + containerSize.height]
+    orientation === 'vertical'
+      ? [0, contentScrollSize.height - containerSize.height]
+      : [0, -contentScrollSize.width - containerSize.width]
   );
-  const before = useTransform(scrollYProgress, [0, 1], [0, contentScrollSize]);
-  const after = useTransform(scrollYProgress, [0, 1], [contentScrollSize, 0]);
   const physics = { damping: 15, mass: 0.27, stiffness: 55 }; // easing of smooth scroll
   const spring = useSpring(transform, physics); // apply easing to the negative scroll value
 
   return (
-    <div className={'max-h-full overflow-y-scroll'} ref={ref}>
-      {/*<motion.div style={{ height: before }} />*/}
+    <div
+      className={classNames(' ', {
+        'overflow-y-scroll max-h-full': orientation === 'vertical',
+        'overflow-x-scroll flex max-w-full': orientation === 'horizontal',
+      })}
+      ref={ref}
+    >
       <div
-        style={{ height: ref.current?.clientHeight ?? '100%' }}
-        className={'overflow-hidden scroll-container top-0 sticky'}
+        style={
+          orientation === 'vertical'
+            ? { height: containerSize.height }
+            : { width: containerSize.width }
+        }
+        className={classNames('overflow-hidden flex-none sticky', {
+          'left-0': orientation === 'horizontal',
+          'top-0': orientation === 'vertical',
+        })}
       >
         <motion.div
           ref={contentRef}
-          style={{ y: spring }} // translateY of scroll container using negative scroll value
+          style={orientation == 'vertical' ? { y: spring } : { x: spring }}
         >
-          {/*{children}*/}
-          <div
-            style={{ height: '200px', width: '800px' }}
-            className={'bg-primary'}
-          />{' '}
-          <div
-            style={{ height: '200px', width: '800px' }}
-            className={'bg-secondary'}
-          />{' '}
-          <div
-            style={{ height: '200px', width: '800px' }}
-            className={'bg-tertiary'}
-          />{' '}
-          <div
-            style={{ height: '200px', width: '800px' }}
-            className={'bg-primary'}
-          />{' '}
-          <div
-            style={{ height: '200px', width: '800px' }}
-            className={'bg-secondary'}
-          />{' '}
+          {children}
         </motion.div>
       </div>
 
-      {/* blank div that has a dynamic height based on the content's inherent height */}
-      {/* this is neccessary to allow the scroll container to scroll... */}
-      {/* ... using the browser's native scroll bar */}
-      {contentScrollSize.height > containerSize.height && (
-        <motion.div style={{ height: contentScrollSize.height }} />
-      )}
+      {orientation === 'vertical' &&
+        contentScrollSize.height > containerSize.height && (
+          <motion.div style={{ height: contentScrollSize.height }} />
+        )}
+
+      {orientation === 'horizontal' &&
+        contentScrollSize.width > containerSize.width && (
+          <motion.div
+            className={'flex-none'}
+            style={{ width: contentScrollSize.width }}
+          />
+        )}
     </div>
   );
 };
