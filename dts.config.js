077329b8f733ea459ad06abd7bbe0228a60f377a
Joël VIGREUX <joel@udixio.fr>
2025-07-10 18:04:16 +0200

    refactor(config): enhance Rollup configuration in DTS setup
    Updated `rollup` function in `dts.config.js` to dynamically mark `lodash-es` as external. This approach ensures broader compatibility and optimizes the configuration by handling various external formats seamlessly.

--- dts.config.js
+++ dts.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  rollup: {
-    external: ['lodash-es'],
+  // This function will run for each entry/format/env combination
+  rollup(config, options) {
+    // Mark 'lodash-es' as external
+    if (!config.external) {
+      config.external = [];
+    } else if (typeof config.external === 'string') {
+      config.external = [config.external];
+    }
+
+    if (Array.isArray(config.external)) {
+      config.external.push('lodash-es');
+    }
+
+    return config; // always return a config.
   },
 };
