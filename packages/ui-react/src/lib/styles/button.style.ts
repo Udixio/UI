5373f39774421508d2c7ca264f2cc48dd4c022e1
Joël VIGREUX <joel@udixio.fr>
2025-07-23 17:13:50 +0200

    refactor(theme): update color palette and refine button styles
    - Enhanced color configurations in `udixio.css` for better tonal consistency and contrast.
    - Updated `filledTonal` button variant to use `bg-secondary-container` in `button.style.ts` to align with the updated theme.

--- packages/ui-react/src/lib/styles/button.style.ts
+++ packages/ui-react/src/lib/styles/button.style.ts
@@ -52,7 +52,7 @@ export const buttonStyle = defaultClassNames<ButtonInterface>(
         'bg-primary hover:shadow-1': !disabled,
       },
       variant === 'filledTonal' && {
-        'hover:shadow-1': !disabled,
+        'bg-secondary-container hover:shadow-1': !disabled,
       },
       variant === 'outlined' && [
         ' border',
