0b020c33757332de7c2439eb9967f9f5e248b25c
Joël VIGREUX <joel@udixio.fr>
2025-07-24 13:13:03 +0200

    refactor(theme): remove unused `Theme` type from Tailwind plugin configuration

--- packages/theme/src/lib/plugins/tailwind/main.ts
+++ packages/theme/src/lib/plugins/tailwind/main.ts
@@ -1,12 +1,5 @@
 import { bootstrapFromConfig } from '../../main';
 import { TailwindPlugin } from './tailwind.plugin';
-import { PluginsConfig } from 'tailwindcss/plugin';
-
-export type Theme = {
-  colors: Record<string, string>;
-  fontFamily: { expressive: string[]; neutral: string[] };
-  plugins: Partial<PluginsConfig>;
-};
 
 export const createTheme = () => {
   const app = bootstrapFromConfig();
