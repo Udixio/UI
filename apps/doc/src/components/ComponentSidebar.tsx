1696f3da87a7dfbf09ea650f640dc7ef341ff7b0
Joël VIGREUX <joel@udixio.fr>
2025-08-21 17:23:31 +0200

    feat(doc): remove `textField.json` and improve `ComponentSidebar` interactivity
    - Deleted unused `textField.json` file to clean up outdated API data.
    - Enhanced `ComponentSidebar` by initializing `activeComponent` state with the `current` prop value.
    - Introduced `useEffect` to synchronize `activeComponent` with the `current` prop for better responsiveness.
    - Simplified slug handling by removing `kebabCase` transformation.

--- apps/doc/src/components/ComponentSidebar.tsx
+++ apps/doc/src/components/ComponentSidebar.tsx
@@ -1,18 +1,25 @@
-import { kebabCase, noCase, sentenceCase } from 'change-case';
+import { noCase, sentenceCase } from 'change-case';
 import { Button } from '@udixio/ui-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const ComponentSidebar = ({
   components,
+  current = null,
 }: {
   components: { slug: string }[];
+  current : string | null
 }) => {
-  const [activeComponent, setActiveComponent] = useState<null | string>(null);
+  const [activeComponent, setActiveComponent] = useState<null | string>(current);
+
+  useEffect(() => {
+    setActiveComponent(current)
+  }, [current]);
+
   return (
     <nav className="sticky top-0 flex flex-col  p-4 bg-surface-container h-screen overflow-auto">
       {components.map(({ slug }) => (
         <Button
-          href={'/components/' + kebabCase(slug)}
+          href={'/components/' + slug}
           className={'justify-start'}
           label={sentenceCase(noCase(slug))}
           onToggle={(value) => {
