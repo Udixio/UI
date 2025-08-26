166d850e45edf33be5012d3f8e30b34a7c6b1c3b
Joël VIGREUX <joel@udixio.fr>
2025-08-26 15:27:18 +0200

    feat(tailwind): add `@tailwindcss/browser` support and improve environment handling
    - Added `@tailwindcss/browser` as a dependency for browser-specific functionality.
    - Refactored `TailwindImplPlugin` to dynamically detect and adapt to Node.js and browser environments.
    - Introduced `detectEnvironment`, `loadFromBrowser`, and `loadFromNode` methods for better execution flow.
    - Enhanced color handling and `outputCss` generation for improved theming and modularity

--- packages/tailwind/src/tailwind.plugin.ts
+++ packages/tailwind/src/tailwind.plugin.ts
@@ -1,10 +1,3 @@
-import {
-  createOrUpdateFile,
-  findProjectRoot,
-  findTailwindCssFile,
-  getFileContent,
-  replaceFileContent,
-} from './file';
 import path from 'path';
 import { FontPlugin, PluginAbstract, PluginImplAbstract } from '@udixio/theme';
 import { ConfigCss } from './main';
@@ -26,13 +19,46 @@ export class TailwindPlugin extends PluginAbstract<
 }
 
 class TailwindImplPlugin extends PluginImplAbstract<TailwindPluginOptions> {
+  private isNode: boolean;
+  public outputCss = '';
+  private colors: Record<
+    string,
+    {
+      light: string;
+      dark: string;
+    }
+  > = {};
+
+  private detectEnvironment(): boolean {
+    return (
+      typeof process !== 'undefined' &&
+      process.versions != null &&
+      process.versions.node != null
+    );
+  }
+
   onInit() {
     this.options.responsiveBreakPoints ??= {
       lg: 1.125,
     };
+    this.isNode = this.detectEnvironment();
   }
 
-  onLoad() {
+  async loadFromBrowser() {
+    await import('@tailwindcss/browser');
+
+    this.loadColor();
+  }
+
+  async loadFromNode() {
+    const {
+      createOrUpdateFile,
+      findProjectRoot,
+      findTailwindCssFile,
+      getFileContent,
+      replaceFileContent,
+    } = await import('./file');
+
     let udixioCssPath = this.options.styleFilePath;
 
     const projectRoot = findProjectRoot(path.resolve());
@@ -49,32 +75,13 @@ class TailwindImplPlugin extends PluginImplAbstract<TailwindPluginOptions> {
       }
     }
 
-    const colors: Record<
-      string,
-      {
-        light: string;
-        dark: string;
-      }
-    > = {};
-
-    for (const isDark of [false, true]) {
-      this.api.themes.update({ isDark: isDark });
-      for (const [key, value] of this.api.colors.getColors().entries()) {
-        const newKey = key
-          .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
-          .toLowerCase();
-        colors[newKey] ??= { light: '', dark: '' };
-        colors[newKey][isDark ? 'dark' : 'light'] = value.getHex();
-      }
-    }
-
     const { fontStyles, fontFamily } = this.api.plugins
       .getPlugin(FontPlugin)
       .getInstance()
       .getFonts();
 
     const configCss: ConfigCss = {
-      colorKeys: Object.keys(colors).join(', '),
+      colorKeys: Object.keys(this.colors).join(', ') as any,
       fontStyles: Object.entries(fontStyles)
         .map(([fontRole, fontStyle]) =>
           Object.entries(fontStyle)
@@ -86,36 +93,21 @@ class TailwindImplPlugin extends PluginImplAbstract<TailwindPluginOptions> {
             )
             .join(', '),
         )
-        .join(', '),
+        .join(', ') as any,
       responsiveBreakPoints: Object.entries(
         this.options.responsiveBreakPoints ?? {},
       )
         .map(([key, value]) => `${key} ${value}`)
-        .join(', '),
+        .join(', ') as any,
     };
 
-    createOrUpdateFile(
-      udixioCssPath,
-      `
-@plugin "@udixio/tailwind" {
+    this.outputCss += `@plugin "@udixio/tailwind" {
   colorKeys: ${configCss.colorKeys};
   fontStyles: ${configCss.fontStyles};
   responsiveBreakPoints: ${configCss.responsiveBreakPoints};
-}
-@custom-variant dark (&:where(.dark, .dark *));
-@theme {
-  --color-*: initial;
-  ${Object.entries(colors)
-    .map(([key, value]) => `--color-${key}: ${value.light};`)
-    .join('\n  ')}
-}
-@layer theme {
-  .dark {
-  ${Object.entries(colors)
-    .map(([key, value]) => `--color-${key}: ${value.dark};`)
-    .join('\n  ')}
-  }
-}
+}`;
+    this.loadColor();
+    this.outputCss += `
 @theme {
   ${Object.entries(fontFamily)
     .map(
@@ -123,8 +115,47 @@ class TailwindImplPlugin extends PluginImplAbstract<TailwindPluginOptions> {
         `--font-${key}: ${values.map((value) => `"${value}"`).join(', ')};`,
     )
     .join('\n  ')}
+}`;
+
+    createOrUpdateFile(udixioCssPath, this.outputCss);
+  }
+
+  loadColor() {
+    this.outputCss += `
+@custom-variant dark (&:where(.dark, .dark *));
+@theme {
+  --color-*: initial;
+  ${Object.entries(this.colors)
+    .map(([key, value]) => `--color-${key}: ${value.light};`)
+    .join('\n  ')}
 }
-`,
-    );
+@layer theme {
+  .dark {
+  ${Object.entries(this.colors)
+    .map(([key, value]) => `--color-${key}: ${value.dark};`)
+    .join('\n  ')}
+  }
+}
+`;
+  }
+
+  async onLoad() {
+    this.colors = {};
+    for (const isDark of [false, true]) {
+      this.api.themes.update({ isDark: isDark });
+      for (const [key, value] of this.api.colors.getColors().entries()) {
+        const newKey = key
+          .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
+          .toLowerCase();
+        this.colors[newKey] ??= { light: '', dark: '' };
+        this.colors[newKey][isDark ? 'dark' : 'light'] = value.getHex();
+      }
+    }
+
+    if (!this.isNode) {
+      await this.loadFromBrowser();
+    } else {
+      await this.loadFromNode();
+    }
   }
 }
