b5f54681ed969a399e1853001c36e049ca0a9ace
Joël VIGREUX <joel@udixio.fr>
2025-09-04 16:31:33 +0200

    fix: change theme variant and remove free-brands-svg-icons

--- apps/doc/theme.config.ts
+++ apps/doc/theme.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig, FontPlugin } from '@udixio/theme';
+import { defineConfig, FontPlugin, Variants } from '@udixio/theme';
 import { TailwindPlugin } from '@udixio/tailwind';
 
 export default defineConfig({
   sourceColor: '#6750A4',
+  variant: Variants.Fidelity,
   plugins: [new FontPlugin({}), new TailwindPlugin({})],
+  colors: (colorService) => ({
+    fromPalettes: ['success'],
+  }),
+  palettes: {
+    success: '#4CAF50',
+  },
 });
